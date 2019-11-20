962baa85aa698516a821d606d2afaee38a331d71
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2019-11-20 17:19:50 +0530

    Updates

---
+++ precache-manifest.be0636ad88dc797c736383ea33f8a160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c592652694928a8a5bd7046d66c42d0",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "89bc2db243030d5cd342",
+    "url": "/monstersRolodex/static/css/main.1f254cc7.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monstersRolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "89bc2db243030d5cd342",
+    "url": "/monstersRolodex/static/js/main.42630c00.chunk.js"
+  },
+  {
+    "revision": "d37f03c25d650b8dab02",
+    "url": "/monstersRolodex/static/js/runtime-main.a8ca2f45.js"
+  }
+]);
